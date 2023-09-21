24af45327b45992e7551caa74b90b8e7c751c246
Diego Villegas <diegon.villegas@gmail.com>
2023-09-21 04:06:15 -0300

    agregue botones y colores

--- js/menu.js
+++ js/menu.js
@@ -2,9 +2,6 @@ const hamburguer = document.querySelector(".hamburguer");
 const menu = document.querySelector(".menu-navegacion");
 const btn_up1 = document.getElementById("boton_up")
 
-// console.log(menu)
-// console.log(hamburguer)
-
 hamburguer.addEventListener('click', ()=> {
   menu.classList.toggle("spread");
   btn_up1.style.display = 'none'
